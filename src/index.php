99fcbad3436ca06a856e4b19f5abc33aa0f99ff5
shami-huawei <75711758+shami-huawei@users.noreply.github.com>
2022-05-19 16:46:36 +0800

    Vodafone Logo Involved

--- src/index.php
+++ src/index.php
@@ -1,7 +1,7 @@
 <?php
 
-echo "Hello, DDA ! <br>";
-echo "Hello from CCE! <br>";
+echo "Hello, Vodafone ! <br>";
+echo "Hello from ServiceStage & CCE! <br>";
 echo '<img src="https://www.mediaoffice.ae/-/media/2021/october/07-10/03/dda-gitex_theme.png?h=2999&w=2964&hash=2AF70BEE3BD0DA9F591C5A48B869A938">';
 
 ?>
