3448862a81246522e89f70046849f082168783db
shami-huawei <75711758+shami-huawei@users.noreply.github.com>
2022-06-24 06:07:48 +0800

    Meezan Bank

--- src/index.php
+++ src/index.php
@@ -1,7 +1,7 @@
 <?php
 
-echo "Hello, Huawei Cloud Presentation Compeitition Judges ! <br>";
+echo "Hello, Meezan Bank ! <br>";
 echo "Hello from ServiceStage & CCE! <br>";
-echo '<img src="https://pbs.twimg.com/media/FTmUmkLWAAISx_x.jpg">';
+echo '<img src="https://www.fintechfutures.com/files/2020/01/meezan.jpg">';
 
 ?>
