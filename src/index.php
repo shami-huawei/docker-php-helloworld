9d49d73e98554ffabe93f0d9e0d0cd9800d90485
shami-huawei <75711758+shami-huawei@users.noreply.github.com>
2022-06-23 18:58:56 +0800

    adding another image

--- src/index.php
+++ src/index.php
@@ -2,6 +2,6 @@
 
 echo "Hello, Huawei Cloud Presentation Compeitition Judges ! <br>";
 echo "Hello from ServiceStage & CCE! <br>";
-echo '<img src="https://support.huaweicloud.com/intl/en-us/productdesc-cce/en-us_image_0000001127367963.png">';
+echo '<img src="https://pbs.twimg.com/media/FTmUmkLWAAISx_x.jpg">';
 
 ?>
