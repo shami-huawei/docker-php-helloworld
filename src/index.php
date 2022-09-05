6f3cb0d6fe881539b4031f3980a71f6354c34cf9
shami-huawei <75711758+shami-huawei@users.noreply.github.com>
2022-09-05 19:59:41 +0800

    Adding Habib Bank

--- src/index.php
+++ src/index.php
@@ -2,6 +2,6 @@
 
 echo "Hello, Meezan Bank ! <br>";
 echo "Hello from ServiceStage & CCE! <br>";
-echo '<img src="https://www.fintechfutures.com/files/2020/01/meezan.jpg">';
+echo '<img src="https://cpecinfo.com/wp-content/uploads/2019/11/Habib-Bank-Limited-800x475.jpg">';
 
 ?>
