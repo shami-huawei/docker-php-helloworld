b0fc066d4fd3f2a94b69780d32faabeb40102ad6
shami-huawei <75711758+shami-huawei@users.noreply.github.com>
2023-08-07 11:17:30 +0800

    trying Span

--- src/index.php
+++ src/index.php
@@ -1,8 +1,10 @@
 <?php
+
+$string= "Welcome! XL!";
+echo $string."<br>";
+
 echo "<br>";
-echo '<span style="color: red; font-size: 50px;">
-echo "Hello, XL ! <br>";
-echo "Hello from ServiceStage & CCE! <br>";
+echo '<span style="color: red; font-size: 50px;"> ' . $string. </span>';
 echo '<img src="https://img.antaranews.com/cache/800x533/2023/08/01/Foto-Bersama-XL-Huawei-Strategic-Collaboration.jpg">';
 
 ?>
