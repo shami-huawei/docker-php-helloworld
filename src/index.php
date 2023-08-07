c666da481cb7286d491edae7216cbb9ae9ad9d59
shami-huawei <75711758+shami-huawei@users.noreply.github.com>
2023-08-07 11:25:22 +0800

    Changing the sequence of Fonts

--- src/index.php
+++ src/index.php
@@ -1,10 +1,9 @@
 <?php
 
 $string= "Welcome! XL!";
-echo $string."<br>";
-
+echo '<img src="https://img.antaranews.com/cache/800x533/2023/08/01/Foto-Bersama-XL-Huawei-Strategic-Collaboration.jpg">';
 echo "<br>";
 echo '<span style="color: red; font-size: 50px;"> ' . $string. ' </span>';
-echo '<img src="https://img.antaranews.com/cache/800x533/2023/08/01/Foto-Bersama-XL-Huawei-Strategic-Collaboration.jpg">';
+
 
 ?>
