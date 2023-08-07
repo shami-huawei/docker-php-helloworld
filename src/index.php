4e775a44eb3852309ed98f43482af2df7e65eeb1
shami-huawei <75711758+shami-huawei@users.noreply.github.com>
2023-08-07 10:59:37 +0800

    Adding color and increase font size

--- src/index.php
+++ src/index.php
@@ -1,5 +1,6 @@
 <?php
-
+echo "<br>";
+echo '<span style="color: red; font-size: 50px;">
 echo "Hello, XL ! <br>";
 echo "Hello from ServiceStage & CCE! <br>";
 echo '<img src="https://img.antaranews.com/cache/800x533/2023/08/01/Foto-Bersama-XL-Huawei-Strategic-Collaboration.jpg">';
