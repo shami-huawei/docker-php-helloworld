d1ef3efb042dd8c63871f60b435db5d5b461ce74
shami-huawei <75711758+shami-huawei@users.noreply.github.com>
2023-08-07 11:18:58 +0800

    Update index.php

--- src/index.php
+++ src/index.php
@@ -4,7 +4,7 @@ $string= "Welcome! XL!";
 echo $string."<br>";
 
 echo "<br>";
-echo '<span style="color: red; font-size: 50px;"> ' . $string. </span>';
+echo '<span style="color: red; font-size: 50px;"> ' . $string. ' </span>';
 echo '<img src="https://img.antaranews.com/cache/800x533/2023/08/01/Foto-Bersama-XL-Huawei-Strategic-Collaboration.jpg">';
 
 ?>
