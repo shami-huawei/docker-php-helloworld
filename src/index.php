0a752d7d79a914a31ec959eb0811e32de6fb5efd
shami-huawei <75711758+shami-huawei@users.noreply.github.com>
2023-08-07 10:49:09 +0800

    Update index.php

--- src/index.php
+++ src/index.php
@@ -2,6 +2,6 @@
 
 echo "Hello, XL ! <br>";
 echo "Hello from ServiceStage & CCE! <br>";
-echo '<img src="https://www.google.com.hk/url?sa=i&url=https%3A%2F%2Fen.antaranews.com%2Fnews%2F289923%2Fxl-axiata-indonesia-huawei-forge-ahead-towards-new-milestone-through-strategy-alignment-summit&psig=AOvVaw0Djo75vQEfKktRtv_UHulw&ust=1691462495086000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCIDh-IHDyYADFQAAAAAdAAAAABAD">';
+echo '<img src="https://img.antaranews.com/cache/800x533/2023/08/01/Foto-Bersama-XL-Huawei-Strategic-Collaboration.jpg">';
 
 ?>
