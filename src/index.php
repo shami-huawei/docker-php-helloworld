6efe83eded5137c993c87fab84bd96e5ab9240b9
shami-huawei <75711758+shami-huawei@users.noreply.github.com>
2023-08-07 10:44:01 +0800

    Adding XL

--- src/index.php
+++ src/index.php
@@ -1,7 +1,7 @@
 <?php
 
-echo "Hello, Meezan Bank ! <br>";
+echo "Hello, XL ! <br>";
 echo "Hello from ServiceStage & CCE! <br>";
-echo '<img src="https://cpecinfo.com/wp-content/uploads/2019/11/Habib-Bank-Limited-800x475.jpg">';
+echo '<img src="https://www.google.com.hk/url?sa=i&url=https%3A%2F%2Fen.antaranews.com%2Fnews%2F289923%2Fxl-axiata-indonesia-huawei-forge-ahead-towards-new-milestone-through-strategy-alignment-summit&psig=AOvVaw0Djo75vQEfKktRtv_UHulw&ust=1691462495086000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCIDh-IHDyYADFQAAAAAdAAAAABAD">';
 
 ?>
